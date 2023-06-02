c0936e46f842d812754621ae1b0851f0a8e81f6a
Marcusvcsilva <marcusv.cordeirosilva@gmail.com>
2023-06-01 22:16:52 -0300

    Update listar.php

--- endpoints/listar.php
+++ endpoints/listar.php
@@ -1,6 +1,15 @@
 <?php
 
 // Verificar se aleatorio=1 ou listar todas:
+header('Content-Type: application/json; charset=utf-8');
+require_once('../classes/Frase.class.php');
+$f = new Frase();
 
+$resultado = $f -> ListarTodas();
+
+// Transformar a array em um Json:
+$json = json_encode($resultado);
+
+echo $json;
 
 ?>
